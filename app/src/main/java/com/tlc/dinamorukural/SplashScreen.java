a31270723c612ecae68de66e715f6620ace7757e
The Little Cormorant <TheLittleCormorant@users.noreply.github.com>
2020-12-27 05:57:38 +0530

    adding launcher icons and a splash screen

--- app/src/main/java/com/tlc/dinamorukural/SplashScreen.java
+++ app/src/main/java/com/tlc/dinamorukural/SplashScreen.java
@@ -2,6 +2,7 @@ package com.tlc.dinamorukural;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
@@ -9,6 +10,14 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.wait(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+
         setContentView(R.layout.activity_splash_screen);
     }
 }
